d4b19fde918e127889a01e66576318a1ac5eef49
Anirban Ghosh <anirbang12427@gmail.com>
2026-03-01 05:03:05 -0800

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 
 import express from "express"
 import dotenv from "dotenv"
+import path from "path"
 import authRouters from "./routes/auth.route.js";
 import messageRouters from "./routes/message.route.js";
 dotenv.config()
@@ -10,6 +11,18 @@ const  app = express();
 
 const PORT =process.env.PORT || 300;
 
+const __dirname = path.resolve();
+
 app.use("/api/auth", authRouters)
 app.use("/api/messages", messageRouters)
+
+
+//make ready for deployment 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend/dist", "dist", "index.html"))
+    })
+}
 app.listen(PORT, ()=> console.log("Sarver runing on port "+ PORT))
