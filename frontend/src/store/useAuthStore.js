132e64688ad09e278e5b953755ab8e3bd19f58cc
Anirban Ghosh <157892015+AnirbanGhosh021@users.noreply.github.com>
2026-03-15 05:41:34 -0700

    Update backend URL for production environment

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client"
  const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000"
-    : "https://chats-1backends.onrender.com";
+    : "https://chatsy-back-h2f0.onrender.com";
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isCheckingAuth: true,
