965813f03c6c5b6cf9ff1557096181ead29e39b5
Anirban Ghosh <157892015+AnirbanGhosh021@users.noreply.github.com>
2026-03-15 05:50:11 -0700

    Fix base URL in axios instance configuration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ export const axiosInstance =  axios.create({
     baseURL:
   import.meta.env.MODE === "development"
     ? " https://chats-1backends.onrender.com/api"
-    : "https://chatsy-back-h2f0.onrender.comapi",
+    : "https://chatsy-back-h2f0.onrender.comapi/ap",
     withCredentials: true
 })
