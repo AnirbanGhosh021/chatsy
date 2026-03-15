0d36d8440371685b657445487db8133b38509b14
Anirban Ghosh <157892015+AnirbanGhosh021@users.noreply.github.com>
2026-03-15 05:22:58 -0700

    Update axios baseURL for development environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 export const axiosInstance =  axios.create({
-    baseURL : import.meta.env.MODE === "development" ? "https://chats-1backends.onrender.com/api" : "/api",
+    baseURL:
+  import.meta.env.MODE === "development"
+    ? " https://chats-1backends.onrender.com/api"
+    : "http://localhost:3000/api",
     withCredentials: true
 })
