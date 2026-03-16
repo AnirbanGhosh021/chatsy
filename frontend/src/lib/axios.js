20eb1e31040bb724bffb2ddc1a306555fe495b09
Anirban Ghosh <157892015+AnirbanGhosh021@users.noreply.github.com>
2026-03-16 08:49:43 -0700

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance =  axios.create({
-    baseURL:  "https://chatsy-backens.onrender.com",
+    baseURL:  "https://chatsy-backens.onrender.com/api",
     withCredentials: true
 })
